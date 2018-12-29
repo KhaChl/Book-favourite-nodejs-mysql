9b5ec4426a36038383aee914e4adf3ea837d8395
Kevin <kpizarro.m@outlook.com>
2018-12-29 14:47:55 -0300

    Validate route register and add confirm password

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -19,11 +19,19 @@ route.get('/register', isNotLoggedIn, (req, res) => {
     res.render('auth/register');
 });
 
-route.post('/register', passport.authenticate('local.register', {
-    successRedirect: '/profile',
-    failureRedirect: '/register',
-    failureFlash: true
-}));
+route.post('/register', (req, res) => {
+    const { username, name, surname, password, confirmpassword } = req.body;
+    req.checkBody('username', 'Username is invalid').matches(/^[a-zA-Z0-9]+$/, "i");
+    req.checkBody('name', 'Name only letters').matches(/^[a-zA-z]+$/, "i");
+    req.checkBody('surname', 'Surname only letters').matches(/^[a-zA-Z]+$/, "i");
+    req.checkBody('password', 'Password use 5 or more characters').matches(/^[a-zA-Z0-9]{5,100}$/, "i");
+    req.checkBody('password', 'Passwords do not match').equals(confirmpassword);
+    passport.authenticate('local.register', {
+        successRedirect: '/profile',
+        failureRedirect: '/register',
+        failureFlash: true
+    })(req, res);
+});
 
 route.get('/profile', isLoggedIn, (req, res) => {
     res.render('profile');
